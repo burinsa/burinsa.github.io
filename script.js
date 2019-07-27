e2d31cb904844f6fd6ccf7441f2e79cce12c0640
Сергей <46132680+burinsa@users.noreply.github.com>
2019-07-27 16:33:22 +0200

    доделал поворот стрелки на аккордион

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ var serviceLaptop = document.querySelector(".laptop");
 var tableLaptop = document.querySelector(".table-laptop");
 var serviceComputer = document.querySelector(".computer");
 var tableComputer = document.querySelector(".table-computer");
-var duration = document.querySelector(".duration");
+var durationPhone = servicePhone.querySelector(".duration");
+var durationPad = servicePad.querySelector(".duration");
+var durationLaptop = serviceLaptop.querySelector(".duration");
+var durationComputer = serviceComputer.querySelector(".duration");
 
 
 
@@ -30,21 +33,23 @@ toTop.onclick = function() {
 servicePhone.addEventListener("click", function (evt) {
     evt.preventDefault();
     tablePhone.classList.toggle("table-show");
-    duration.classList.toggle("rotate");
+    durationPhone.classList.toggle("rotate");
 });
 
 servicePad.addEventListener("click", function (evt) {
     evt.preventDefault();
     tablePad.classList.toggle("table-show");
-    duration.classList.toggle("rotate");
+    durationPad.classList.toggle("rotate");
 });
 
 serviceLaptop.addEventListener("click", function (evt) {
     evt.preventDefault();
     tableLaptop.classList.toggle("table-show");
+    durationLaptop.classList.toggle("rotate");
 });
 
 serviceComputer.addEventListener("click", function (evt) {
     evt.preventDefault();
     tableComputer.classList.toggle("table-show");
+    durationComputer.classList.toggle("rotate");
 });
